653a34c60900b11c2ba8a7f29f490afd439adee9
chinexdoc-beep <chinexdoc@gmail.com>
2026-08-14 04:02:13 +0100

    Fix Nodemailer IPc6 and port settings for Render

--- server.js
+++ server.js
@@ -72,11 +72,14 @@ const Report = mongoose.model('Report', reportSchema);
 // NODEMAILER TRANSPORTER & EMAIL HELPERS
 // -------------------------------------------------------------
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  family: 4
 });
 
 // 1. WELCOME EMAIL
